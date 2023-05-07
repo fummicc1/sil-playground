790a93c322228a711d467c87f86d74c7aea46cf3
fummicc1 <fumiya.tennis1@gmail.com>
2023-05-07 15:50:44 +0900

    Fix

--- backdeployed.swift
+++ backdeployed.swift
@@ -2,7 +2,7 @@ public struct Printer {
     @available(macOS 12, *)
     @backDeployed(before: macOS 13)
     public func call() {
-        print("macOS 12以上macOS 13未満")
+        print("macOS 12以上macOS 13未満ではなくmacOS 12以上のAPIになる")
     }
 }
 
